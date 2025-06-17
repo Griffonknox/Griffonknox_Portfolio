0b9fa545c81a9f7936b9804817c1423918c86292
griffonknox <thompsonscott1180@gmail.com>
2025-06-17 16:09:10 -0500

    finished portfolio and added skateboarding

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { ResumeComponent } from './pages/resume/resume.component';
 import { PortfolioComponent } from './pages/portfolio/portfolio.component';
+import { SkateboardingComponent } from './pages/skateboarding/skateboarding.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'resume', component: ResumeComponent },
   { path: 'portfolio', component: PortfolioComponent },
+  { path: 'skateboarding', component: SkateboardingComponent },
   { path: '**', redirectTo: '' },
 ];
---
+++ src/app/pages/skateboarding/skateboarding.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-skateboarding',
+  imports: [],
+  templateUrl: './skateboarding.component.html',
+  styleUrl: './skateboarding.component.css'
+})
+export class SkateboardingComponent {
+
+}
