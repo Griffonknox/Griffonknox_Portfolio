8563d068dfcc0caf6f02153d121fae88b7e35c80
griffonknox <thompsonscott1180@gmail.com>
2025-09-10 12:42:12 -0500

    outside the code

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,12 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { ResumeComponent } from './pages/resume/resume.component';
 import { PortfolioComponent } from './pages/portfolio/portfolio.component';
-import { SkateboardingComponent } from './pages/skateboarding/skateboarding.component';
+import { OutsidethecodeComponent } from './pages/outsidethecode/outsidethecode.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'resume', component: ResumeComponent },
   { path: 'portfolio', component: PortfolioComponent },
-  { path: 'skateboarding', component: SkateboardingComponent },
+  { path: 'outsidethecode', component: OutsidethecodeComponent },
   { path: '**', redirectTo: '' },
 ];
---
+++ src/app/pages/outsidethecode/outsidethecode.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-outsidethecode',
+  imports: [],
+  templateUrl: './outsidethecode.component.html',
+  styleUrl: './outsidethecode.component.css'
+})
+export class OutsidethecodeComponent {
+
+}
