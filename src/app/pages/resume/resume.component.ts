19508ff329ddb6262bc7eb883b6b50db6a91e38f
griffonknox <thompsonscott1180@gmail.com>
2025-09-19 12:48:12 -0500

    updated resume toggle

--- src/app/pages/resume/resume.component.ts
+++ src/app/pages/resume/resume.component.ts
@@ -1,11 +1,33 @@
 import { Component } from '@angular/core';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-resume',
-  imports: [],
+  standalone: true,       // ✅ required
+  imports: [CommonModule], 
   templateUrl: './resume.component.html',
   styleUrl: './resume.component.css'
 })
 export class ResumeComponent {
 
+  selectedResume: 'fullstack' | 'backend' = 'fullstack';
+  resumeUrl!: SafeResourceUrl;
+
+  constructor(private sanitizer: DomSanitizer) {
+    this.setResumeUrl();
+  }
+
+  toggleResume() {
+    this.selectedResume = this.selectedResume === 'fullstack' ? 'backend' : 'fullstack';
+    this.setResumeUrl();
+  }
+
+  setResumeUrl() {
+    const url =
+      this.selectedResume === 'fullstack'
+        ? './images/ScottResumeFullStack.pdf'
+        : './images/ScottResumeBackEnd.pdf';
+    this.resumeUrl = this.sanitizer.bypassSecurityTrustResourceUrl(url);
+  }
 }
